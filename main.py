f7a5e74534a3692de18c5e8d7dabf1df179d9cea
blxxdclxud <ramazanatzuf10@gmail.com>
2021-10-27 18:14:41 +0300

    first_pages.ui finished.

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from main_page import Ui_MainPage
 
 
-class MyWidget(QWidget):
+class MyWidget(QWidget, Ui_MainPage):
     def __init__(self):
         super().__init__()
-        uic.loadUi('first_pages.ui', self)
+        self.setupUi(self)
 
-        self.sign_up_button.clicked.connect(self.run)
-        self.go_back_button.clicked.connect(self.back)
-
-    def run(self):
-        self.stacked_widget.setCurrentWidget(self.sign_up_widget)
-        self.create_email.setFocus()
-
-    def back(self):
-        self.stacked_widget.setCurrentWidget(self.sign_in_widget)
+    #     self.sign_up_button.clicked.connect(self.run)
+    #     self.go_back_button.clicked.connect(self.back)
+    #
+    # def run(self):
+    #     self.stacked_widget.setCurrentWidget(self.sign_up_widget)
+    #     self.create_email.setFocus()
+    #
+    # def back(self):
+    #     self.stacked_widget.setCurrentWidget(self.sign_in_widget)
 
 
 def except_hook(cls, exception, traceback):
